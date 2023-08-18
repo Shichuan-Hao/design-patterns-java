e700b0ab66debec2d0867d24874c81e7baa85f25
haoshichuan <haoshichuan@ipplus360.com>
2023-08-18 14:17:03 +0800

    初始化项目

---
+++ src/test/java/cn/bytewalk/dpj/DesignPatternsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bytewalk.dpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
